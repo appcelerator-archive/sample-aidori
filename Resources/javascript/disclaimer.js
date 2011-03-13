655a8bdba4785686d496f9301e8cb70b1e3f6db8
fusion94 <tguntharp@appcelerator.com>
2011-03-12 16:40:02 -0800

    Disclaimer cleanup to reflect the crisis in Japan

--- Resources/javascript/disclaimer.js
+++ Resources/javascript/disclaimer.js
@@ -5,7 +5,6 @@ var properties = Ti.App.Properties;
 var bgImage = Ti.UI.createImageView({
   top:0,
   left:0,
-  url:'../images/about.png',
   height:'auto',
   width:'auto'
 });
@@ -25,10 +24,9 @@ var disclaimerLabel = Ti.UI.createLabel({
   height: 'auto',
   color: '#fff',
 	font:{fontSize:13, fontWeight:'normal'},
-  text: "1) Do Not Touch anything you observe! Reporting means observing from a safe location. Do not take risk to yourself, others or the wildlife. Reporting is the act of sharing what you see. Please report critical information directly to the authorities. If you find oiled wildlife please call (866) 448-5816. If you find an oiled beach please report it at (866) 448-5816.\n\n" +
-        "2) Data you submit, including your location will be public data. The information collected here via Oil Reporter will be shared back out to the public via a data feed on www.oilreporter.org. It is critical that you turn your location on (see video). Without it, we can't map where you took your picture. Your data will be housed at San Diego State University.\n\n" +
-        "3) Personal privacy. Your personal information will not be collected on this application. We encourage thoughtful reporting. Data retrieved on this application will be monitored. As a reminder, your location and the data you submit will be made public.\n\n" +
-        "If you at any time wish to contact CrisisCommons, please email us at oilreporter@crisiscommons.org\n\n" +
+  text: "CrisisCommons, along with our friends in the greater volunteer technology community, stands ready to provide opportunities to volunteers of all skills levels to contribute to support requests made by crisis response agencies such as UN OCHA, Red Cross or local emergency management in support of their response and recovery efforts to response to 2011 Sendai earthquake and the subsequent tsunami that has affected countries across the Pacific region." +
+        "Personal privacy. Your personal information will not be collected on this application. We encourage thoughtful reporting. Data retrieved on this application will be monitored. As a reminder, your location and the data you submit will be made public.\n\n" +
+        "If you at any time wish to contact CrisisCommons, please email us at NEED_EMAIL_ADDRESS@crisiscommons.org\n\n" +
         "Privacy Statement (crisiscommons.org/privacy)\n\nTerms of Service (crisiscommons.org/tos)"
 });
 scrollView.add(disclaimerLabel);
