6be713c8a14c7c27c367b935b4fd4e0226153c5c
Justin Lee <lis186@gmail.com>
2011-03-16 16:29:29 +0800

    remove unused config button.

--- Resources/javascript/instagram.js
+++ Resources/javascript/instagram.js
@@ -5,9 +5,6 @@ var cc ={win:Ti.UI.currentWindow};
 	cc.refreshButton = Ti.UI.createButton({systemButton:Ti.UI.iPhone.SystemButton.REFRESH});
 	if (Ti.Platform.name != 'android') {
 		Ti.UI.currentWindow.setLeftNavButton(cc.refreshButton);
-		cc.configButton = Ti.UI.createButton({
-			image: '../images/light_gear.png'
-		});
 	};
 	
 	var xhr = Titanium.Network.createHTTPClient();
@@ -24,32 +21,59 @@ var cc ={win:Ti.UI.currentWindow};
 		contentHeight:'auto',
 		backgroundColor: '#000'
 	});
-
+	
+	var detailView = Titanium.UI.createScrollableView({
+		top: 0,
+		left: 0,
+		width: Ti.Platform.displayCaps.platformWidth,
+		height: Ti.Platform.displayCaps.platformHeight,
+		backgroundColor: 'red',
+		visible: false
+	});
+	scrollView.addEventListener('singletap',function(e)
+	{
+		Ti.API.log(e);
+	});
 	Ti.UI.currentWindow.add(containerView);
 	containerView.add(scrollView);
+	containerView.add(detailView);
 
 	cc.retrieveInstagramFeed = function()
 	{
 		xhr.onload = function()
 		{
 			var data=JSON.parse(this.responseText);
+			/*
 			Ti.API.log(this.responseText);
 			Ti.API.log("===============================");
 			Ti.API.log(data);
-
+			*/
 			var i, length = data.data.length, picture;
 			for(i = 0; i<length; i++)
 			{
 				picture = data.data[i];
-				Ti.API.log(picture);
-				var image = Ti.UI.createImageView({
+				//Ti.API.log(picture);
+				var thumbnail = Ti.UI.createImageView({
 					width: Ti.Platform.displayCaps.platformWidth/2,
 					height: Ti.Platform.displayCaps.platformWidth/2,
+					defaultImage: '../images/default_image.png',
 					image: picture.images.thumbnail.url
 				});
-				scrollView.add(image);
+				scrollView.add(thumbnail);
+				
+				var standard_resolution = Ti.UI.createImageView({
+					width: Ti.Platform.displayCaps.platformWidth,
+					height: Ti.Platform.displayCaps.platformWidth,
+					defaultImage: '../images/default_image.png',
+					image: picture.images.standard_resolution.url
+				});
+				
+				detailView.views[i] = standard_resolution;
+				
+				
 			}
 			Ti.App.fireEvent('hide_indicator',{});
+			
 		};
 		// open the client
 		xhr.open('GET','https://api.instagram.com/v1/tags/prayforjapan/media/recent?client_id=067b035e96cf459a9d0db4b66d6d1cf3');
